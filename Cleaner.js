d733fce4480e628ee519dc6c3031411fea763e2f
Artem Orlov <36341862+NULLPOINTER-Artem@users.noreply.github.com>
2020-09-03 04:38:41 +0200

    Clean empty objs

---
+++ Cleaner.js
@@ -0,0 +1,48 @@
+let massive = [
+    Car = {}, 
+    Mobile = {}, 
+    Person = {
+        name: 'Peter',
+        age: 18,
+    },
+    Google = {},
+    Apple = {
+        year: '2010',
+        owner: 'Stive Jobs',
+    }
+];
+
+let massiveWithoutEmptyObjects = cleanEmptyObjects(massive);
+
+console.log(massiveWithoutEmptyObjects);
+console.log(massive);
+
+/**
+ * Функция удаляет пустые объекты из массива объектов
+ * 
+ * @param {object} array массив объектов
+ * @returns Возвращает измененный массив объектов 
+ */
+function cleanEmptyObjects(array) {
+    for (let i = array.length - 1; i >= 0; i--) {
+        if (isEmpty(array[i])) {
+            array.splice(i, 1);
+        }
+    }
+
+    return array;
+};
+
+/**
+ * Функция определяет есть-ли у объекта свои свойства, если нету - объект пуст!
+ * 
+ * @param {object} obj объект
+ * @returns true/false зависимо от выполнения условия 
+ */
+function isEmpty(obj) {
+    if (Object.keys(obj).length === 0) {
+        return true;
+    }
+
+    return false;
+};
